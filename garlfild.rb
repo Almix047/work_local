ce1cf566ea57e3353f58564a499b3a7cdeb0f39e
Almix047 <Amix047@gmail.com>
2020-05-05 20:50:38 +0300

    divided parser into methods

--- garlfild.rb
+++ garlfild.rb
@@ -6,27 +6,9 @@ class CustomParser_13829 < Scripting::CustomParser
     initialize_instance_variables(ctx)
     if product_page?
       if multi_products.any?
-        (0..multi_products.length - 1).each do |num|
-          p = @ctx.base_product.dup
-          p[NAME] = prepare_json.dig('OFFERS', num, 'NAME').split.join(' ')
-          p[SKU] = multi_products[num].xpath('./@data-art').text
-          p[PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'PRICE') if product_availability?(num)
-          p[PROMO_NAME] = 'Акция' if promo?(num)
-          p[REGULAR_PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?(num)
-          p[STOCK] = product_availability?(num)
-          p[KEY] = multi_products[num].xpath('./@data-onevalue').text + p[SKU]
-          ctx.add_product(p)
-        end
+        create_multi_product
       else
-        p = @ctx.base_product.dup
-        p[NAME] = @doc.xpath('//h1[@class="bx-title"]').text
-        p[SKU] = @doc.xpath('//span[@class="item_art_number"]').text
-        p[PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'PRICE') if product_availability?
-        p[PROMO_NAME] = 'Акция' if promo?
-        p[REGULAR_PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?
-        p[STOCK] = product_availability?
-        p[KEY] = @doc.xpath('//input[@name="good_id"]/@value').text + p[SKU]
-        ctx.add_product(p)
+        create_single_product
       end
     end
     ctx.products.any?
@@ -43,6 +25,32 @@ class CustomParser_13829 < Scripting::CustomParser
 
   def multi_products
     @_multi_products ||= @doc.xpath("//div[@class='product-item-detail-info-section']//li")
+  end
+
+  def create_single_product
+    p = @ctx.base_product.dup
+    p[NAME] = @doc.xpath('//h1[@class="bx-title"]').text
+    p[SKU] = @doc.xpath('//span[@class="item_art_number"]').text
+    p[PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'PRICE') if product_availability?
+    p[PROMO_NAME] = 'Акция' if promo?
+    p[REGULAR_PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?
+    p[STOCK] = product_availability?
+    p[KEY] = @doc.xpath('//input[@name="good_id"]/@value').text + p[SKU]
+    @ctx.add_product(p)
+  end
+
+  def create_multi_product
+    (0..multi_products.length - 1).each do |num|
+      p = @ctx.base_product.dup
+      p[NAME] = prepare_json.dig('OFFERS', num, 'NAME').split.join(' ')
+      p[SKU] = multi_products[num].xpath('./@data-art').text
+      p[PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'PRICE') if product_availability?(num)
+      p[PROMO_NAME] = 'Акция' if promo?(num)
+      p[REGULAR_PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?(num)
+      p[STOCK] = product_availability?(num)
+      p[KEY] = multi_products[num].xpath('./@data-onevalue').text + p[SKU]
+      @ctx.add_product(p)
+    end
   end
 
   def prepare_json
