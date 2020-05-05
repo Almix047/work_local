c0db0709f72217d30c6b9ac2b28dcf9cb9f4f246
Almix047 <Amix047@gmail.com>
2020-05-05 14:52:31 +0300

    sync

--- custom_methods.rb
+++ custom_methods.rb
@@ -21,13 +21,13 @@ end
 
 def parse
   puts "Product page: #{product_page?}"
-    p = @ctx.base_product.dup
   if multi_products.any?
     (0..multi_products.length - 1).each do |num|
-  puts '---------------------------------------------------------------------'
+      p = @ctx.base_product.dup
+      puts '---------------------------------------------------------------------'
       puts p[NAME] = prepare_json['OFFERS'][num]['NAME'].split.join(' ')
       puts p[SKU] = multi_products[num].xpath('./@data-art').text
-      puts p[PRICE] = prepare_json['OFFERS'][num]['ITEM_PRICES'].first['PRICE']
+      puts p[PRICE] = prepare_json['OFFERS'][num]['ITEM_PRICES'].first['PRICE'] if product_availability?(num)
       puts p[PROMO_NAME] = 'Акция' if promo?(num)
       puts p[REGULAR_PRICE] = prepare_json['OFFERS'][num]['ITEM_PRICES'].first['BASE_PRICE'] if promo?(num)
       puts p[STOCK] = product_availability?(num)
@@ -36,10 +36,11 @@ def parse
       p.clear
     end
   else
-  puts '---------------------------------------------------------------------'
+    p = @ctx.base_product.dup
+    puts '---------------------------------------------------------------------'
     puts p[NAME] = @doc.xpath('//h1[@class="bx-title"]').text
     puts p[SKU] = @doc.xpath('//span[@class="item_art_number"]').text
-    puts p[PRICE] = prepare_json['PRODUCT']['ITEM_PRICES'].first['PRICE']
+    puts p[PRICE] = prepare_json['PRODUCT']['ITEM_PRICES'].first['PRICE'] if product_availability?
     puts p[PROMO_NAME] = 'Акция' if promo?
     puts p[REGULAR_PRICE] = prepare_json['PRODUCT']['ITEM_PRICES'].first['BASE_PRICE'] if promo?
     puts p[STOCK] = product_availability?
@@ -57,12 +58,36 @@ end
   end
 
   def multi_products
-    @multi_products ||= @doc.xpath("//div[@class='product-item-detail-info-section']//li")
+    @_multi_products ||= @doc.xpath("//div[@class='product-item-detail-info-section']//li")
   end
 
   def promo?(num = nil)
     promo = multi_products.any? ? prepare_json['OFFERS'][num] : prepare_json['PRODUCT']
     promo['ITEM_PRICES'].first['DISCOUNT'].to_f.positive?
+  end
+
+  def product_availability?(num = nil)
+    stock = multi_products.any? ? multi_products[num].xpath('./@data-availstatus').text : @doc.xpath('//@data-availstatus').text
+    stock !~ /not?_avail/
+  end
+
+# -----
+# -----
+# -----
+
+  def key_test_value?
+    if multi_products.empty?
+      sku = @doc.xpath('//span[@class="item_art_number"]').text
+      v_old = @doc.xpath('//p[@class="field js_class_valid"]/input[@name="good_id"]/@value').text + sku
+      v_new = @doc.xpath('//input[@name="good_id"]/@value').text + sku
+      v_old == v_new
+    end
+  end
+
+  def avail_test_value?(num = nil)
+    v_old = product_availability_old?(num)
+    v_new = product_availability?(num)
+    v_old == v_new
   end
 
   def product_availability_old?(num = nil)
@@ -81,28 +106,4 @@ end
       # 'In stock'
       true
     end
-  end
-
-# -----
-# -----
-# -----
-
-  def product_availability?(num = nil)
-    stock = multi_products.any? ? multi_products[num].xpath('./@data-availstatus').text : @doc.xpath('//@data-availstatus').text
-    stock !~ /not?_avail/
-  end
-
-  def key_test_value?
-    if multi_products.empty?
-      sku = @doc.xpath('//span[@class="item_art_number"]').text
-      v_old = @doc.xpath('//p[@class="field js_class_valid"]/input[@name="good_id"]/@value').text + sku
-      v_new = @doc.xpath('//input[@name="good_id"]/@value').text + sku
-      v_old == v_new
-    end
-  end
-
-  def avail_test_value?(num = nil)
-    v_old = product_availability_old?(num)
-    v_new = product_availability?(num)
-    v_old == v_new
   end
