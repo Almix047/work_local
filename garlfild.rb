90b87ab995f06da18ac0243090a4c54594fd99a3
Almix047 <Amix047@gmail.com>
2020-05-08 08:40:58 +0300

    5

--- garlfild.rb
+++ garlfild.rb
@@ -1,17 +1,21 @@
-class CustomParser_13829 < Scripting::CustomParser
-  # def handle404
-  # end
+class EducationGarfiled < Scripting::CustomParser
+  def handle404(ctx)
+    doc = RuleTools.doc(ctx.page_content)
+    if doc.xpath('//div[@class="title_404"]/p').text.include?('Страница не найдена')
+      p = {}
+      p[:url] = ctx.todo[:url]
+      p[DELISTED] = true
+      ctx.products << p
+      true
+    end
+    false
+  end
 
   def parse(ctx)
     initialize_instance_variables(ctx)
-    if product_page?
-      if multi_products.any?
-        create_multi_product
-      else
-        create_single_product
-      end
-    end
-    ctx.products.any?
+    create_multi_product if product_page? && multi_products.any?
+    create_single_product if product_page? && multi_products.empty?
+    @ctx.products.any?
   end
 
   def initialize_instance_variables(ctx)
@@ -20,11 +24,11 @@ class CustomParser_13829 < Scripting::CustomParser
   end
 
   def product_page?
-    @ctx.doc.xpath("//div[@itemtype='//schema.org/Product']").any?
+    @ctx.doc.xpath('//div[@itemtype="//schema.org/Product"]').any?
   end
 
   def multi_products
-    @_multi_products ||= @doc.xpath("//div[@class='product-item-detail-info-section']//li")
+    @_multi_products ||= @doc.xpath('//div[@class="product-item-detail-info-section"]//li')
   end
 
   def create_single_product
@@ -33,22 +37,23 @@ class CustomParser_13829 < Scripting::CustomParser
     p[SKU] = @doc.xpath('//span[@class="item_art_number"]').text
     p[PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'PRICE') if product_availability?
     p[PROMO_NAME] = 'Акция' if promo?
-    p[REGULAR_PRICE] = prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?
+    p[REGULAR_PRICE] = regular_price if promo? && product_availability?
     p[STOCK] = product_availability?
     p[KEY] = @doc.xpath('//input[@name="good_id"]/@value').text + p[SKU]
     @ctx.add_product(p)
   end
 
   def create_multi_product
-    (0..multi_products.length - 1).each do |num|
+    multi_products.each do |product|
       p = @ctx.base_product.dup
-      p[NAME] = prepare_json.dig('OFFERS', num, 'NAME').split.join(' ')
-      p[SKU] = multi_products[num].xpath('./@data-art').text
-      p[PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'PRICE') if product_availability?(num)
-      p[PROMO_NAME] = 'Акция' if promo?(num)
-      p[REGULAR_PRICE] = prepare_json.dig('OFFERS', num, 'ITEM_PRICES', 0, 'BASE_PRICE') if promo?(num)
-      p[STOCK] = product_availability?(num)
-      p[KEY] = multi_products[num].xpath('./@data-onevalue').text + p[SKU]
+      index = find_index_product(product.xpath('./@data-onevalue').text)
+      p[NAME] = prepare_json.dig('OFFERS', index, 'NAME').split.join(' ')
+      p[SKU] = product.xpath('./@data-art').text
+      p[PRICE] = prepare_json.dig('OFFERS', index, 'ITEM_PRICES', 0, 'PRICE') if product_availability?(product)
+      p[PROMO_NAME] = 'Акция' if promo?(product)
+      p[REGULAR_PRICE] = regular_price(index, product) if promo?(product) && product_availability?(product)
+      p[STOCK] = product_availability?(product)
+      p[KEY] = product.xpath('./@data-onevalue').text + p[SKU]
       @ctx.add_product(p)
     end
   end
@@ -59,18 +64,38 @@ class CustomParser_13829 < Scripting::CustomParser
     JSON.parse(data)
   end
 
-  def promo?(num = nil)
-    promo = multi_products.any? ? prepare_json.dig('OFFERS', num) : prepare_json['PRODUCT']
-    promo.dig('ITEM_PRICES', 0, 'DISCOUNT').to_f.positive?
+  def promo?(product = nil)
+    multi_products.any? ? product.xpath('./@data-discountstatus').text.include?('discount') : @doc.xpath('//div[@class="item_discount"]').any?
   end
 
-  def product_availability?(num = nil)
-    stock = multi_products.any? ? multi_products[num].xpath('./@data-availstatus').text : @doc.xpath('//@data-availstatus').text
+  def product_availability?(product = nil)
+    stock = multi_products.any? ? product.xpath('./@data-availstatus').text : @doc.xpath('//@data-availstatus').text
     stock !~ /not?_avail/
   end
 
-  # def promo_conditions
-  #   if multi_products.length.positive?
-  #     multi_products[num].xpath('./@data-discountstatus')
-  # end
+  def find_index_product(pid)
+    pids = prepare_json['TREE_PROPS'].first['VALUES'].map { |key, _val| key }
+    pids.delete('0') # Optional string
+    pids.index(pid)
+  end
+
+  def base_price(index = nil)
+    if multi_products.any?
+      source = prepare_json.dig('OFFERS', index, 'ITEM_PRICES', 0)
+      regular_price = source['BASE_PRICE']
+      price = source['PRICE']
+      if regular_price == price
+        prepare_json['OFFERS'].map { |offer| offer['ITEM_PRICES'].first['BASE_PRICE'] }.max
+      else
+        regular_price
+      end
+    else
+      prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'BASE_PRICE')
+    end
+  end
+
+  def regular_price(index = nil, product = nil)
+    price = multi_products.any? ? product.xpath('./@data-old-price').text.tr(',', '.').to_f : prepare_json.dig('PRODUCT', 'ITEM_PRICES', 0, 'BASE_PRICE').to_f
+    price.positive? ? price : base_price(index)
+  end
 end
